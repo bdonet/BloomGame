430478e2015817c2566918dd7c402239ae157618
Ben Donet <ben.donet@outlook.com>
2023-08-19 13:23:58 -0500

    Added Map object to hold soil grid in memory

---
+++ BloomPrototype/BloomPrototype/GameTypes/Map.cs
@@ -0,0 +1,32 @@
+using BloomPrototype.GameTypes.Soils;
+
+namespace BloomPrototype.GameTypes;
+
+public class Map
+{
+    public Map()
+    {
+        Grid = new Soil[100, 100];
+    }
+
+    private const int ViewSize = 5;
+
+    private Soil[,] Grid;
+
+    public Soil GetSoil(int x, int y) => Grid[x, y];
+    
+    public Soil[,] GetView(int startX, int startY)
+    {
+        var result = new Soil[ViewSize, ViewSize];
+
+        for (int x = 0; x < ViewSize; x++)
+        {
+            for (int y = 0; y < ViewSize; y++)
+            {
+                result[x, y] = Grid[startX + x, startY + y];
+            }
+        }
+
+        return result;
+    } 
+}
