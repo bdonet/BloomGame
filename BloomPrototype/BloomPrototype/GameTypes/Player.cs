35eeb8654f95660d06d4c68e272ceac34d7bde35
Ben Donet <ben.donet@outlook.com>
2023-11-28 18:58:15 -0600

    Added bones of Player class
    Wasn't able to finish because my comuter froze

---
+++ BloomPrototype/BloomPrototype/GameTypes/Player.cs
@@ -0,0 +1,33 @@
+﻿using BloomPrototype.GameTypes.Soils;
+
+namespace BloomPrototype.GameTypes;
+
+public class Player
+{
+	public Player(Map map)
+	{
+		//Location = map.GetSoil(1, 1);
+	}
+
+	public Soil Location { get; private set; }
+
+	public void MoveUp()
+	{
+
+	}
+
+	public void MoveDown()
+	{
+
+	}
+
+	public void MoveLeft()
+	{
+
+	}
+
+	public void MoveRight()
+	{
+
+	}
+}
---
+++ BloomPrototype/Tests.Unit/PlayerTest/ctor.cs
@@ -0,0 +1,34 @@
+﻿using BloomPrototype.GameTypes;
+using BloomPrototype.GameTypes.Soils;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Unit.PlayerTest;
+public class ctor
+{
+	[Fact]
+	public void ctor_GeneralCall_SetsPlayerLocationToSoilAtX1Y1()
+	{
+		/// Arrange
+		var soilMap = new Soil[2, 2];
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				soilMap[i, j] = new Soil();
+			}
+		}
+
+		var map = new Map(soilMap);
+
+		/// Act
+		var player = new Player(map);
+
+		/// Assert
+		player.Location.ShouldBe(soilMap[1, 1]);
+	}
+}
