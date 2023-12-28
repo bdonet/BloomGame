5e855d6f5864a817a305e4561f5ccd0a7acefb39
Ben Donet <brjd345@gmail.com>
2023-12-28 16:40:04 -0600

    Added test for Map.GetSoil

---
+++ BloomPrototype/Tests.Unit/MapTests/GetSoilTest.cs
@@ -0,0 +1,26 @@
+﻿using Shouldly;
+using System;
+using System.Linq;
+using Tests.Utilities;
+
+namespace Tests.Unit.MapTests;
+
+public class GetSoilTest
+{
+	[Theory]
+	[InlineData(0, 0)]
+	[InlineData(0, 1)]
+	[InlineData(1, 0)]
+	[InlineData(1, 1)]
+	public void GetSoil_GeneralCall_ReturnsSoilAtGivenCoordinates(int x, int y)
+	{
+		/// Arrange
+		var map = MapHelper.SetupTestMap(2);
+
+		/// Act
+		var result = map.GetSoil(x, y);
+
+		/// Assert
+		result.ShouldBe(map.Grid[x, y]);
+	}
+}
