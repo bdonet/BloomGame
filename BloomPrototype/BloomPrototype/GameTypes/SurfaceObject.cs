15a231ba258e03a4fcb428a3ab8afbebc1e04e24
Ben Donet <brjd345@gmail.com>
2023-12-28 12:44:47 -0600

    Added new SurfaceObject to control object location on map

---
+++ BloomPrototype/BloomPrototype/GameTypes/SurfaceObject.cs
@@ -0,0 +1,10 @@
+﻿namespace BloomPrototype.GameTypes;
+
+public abstract class SurfaceObject
+{
+	public SurfaceObject(Map map, uint locationX, uint locationY) { }
+
+	protected readonly Map Map;
+	protected uint LocationX;
+	protected uint LocationY;
+}
