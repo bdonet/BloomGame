9f0000f682ad67be0a2db7253dbd03b945beef18
Ben Donet <brjd345@gmail.com>
2023-12-28 14:47:56 -0600

    Cleaned up Plant object a little more

--- BloomPrototype/BloomPrototype/GameTypes/Plants/Plant.cs
+++ BloomPrototype/BloomPrototype/GameTypes/Plants/Plant.cs
@@ -1,10 +1,6 @@
-using BloomPrototype.GameTypes.Soils;
-
 namespace BloomPrototype.GameTypes.Plants;
 
 public abstract class Plant
 {
-	protected Soil HostSoil { get; set; }
-
 	protected PlantMaturity Maturity { get; set; }
 }
