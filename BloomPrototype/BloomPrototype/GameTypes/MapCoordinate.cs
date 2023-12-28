075f59a214fee28325585a825fced0368759bf36
Ben Donet <brjd345@gmail.com>
2023-12-28 15:57:21 -0600

    Created MapCoordinate value object to control coordinate logic

---
+++ BloomPrototype/BloomPrototype/GameTypes/MapCoordinate.cs
@@ -0,0 +1,16 @@
+﻿namespace BloomPrototype.GameTypes;
+
+public class MapCoordinate
+{
+	public MapCoordinate(int x, int y)
+	{
+		if (x < 0 || y < 0)
+			throw new ArgumentException($"Cannot create {nameof(MapCoordinate)} with negative coordinates");
+
+		X = x;
+		Y = y;
+	}
+
+	public int X;
+	public int Y;
+}
---
+++ BloomPrototype/Tests.Unit/MapCoordinateTests/ctorTests.cs
@@ -0,0 +1,46 @@
+﻿using BloomPrototype.GameTypes;
+using Shouldly;
+using System;
+using System.Linq;
+using Tests.Utilities;
+
+namespace Tests.Unit.MapCoordinateTests;
+
+public class ctorTests
+{
+	[Theory]
+	[InlineData(0, 0)]
+	[InlineData(1, 2)]
+	[InlineData(16, 81)]
+	[InlineData(int.MaxValue, int.MaxValue)]
+	public void ctor_GivenCoordinatesAreValid_CreatedObjectHasGivenCoordinates(int expectedX, int expectedY)
+	{
+		/// Arrange
+
+		/// Act
+		var result = new MapCoordinate(expectedX, expectedY);
+
+		/// Assert
+		result.X.ShouldBe(expectedX);
+		result.Y.ShouldBe(expectedY);
+	}
+
+	[Theory]
+	[InlineData(-4, 16)]
+	[InlineData(8, -21)]
+	[InlineData(-2, -8)]
+	public void ctor_AtLeastOneGivenCoordinateIsNegative_ThrowsExceptionWithMessage(int x, int y)
+	{
+		/// Arrange
+
+		/// Act
+		var exception = Record.Exception(() => new MapCoordinate(x, y));
+
+		/// Assert
+		exception.ShouldNotBeNull();
+		exception.ShouldBeOfType<ArgumentException>();
+		exception.Message.ShouldContain(nameof(MapCoordinate));
+		exception.Message.ShouldContain("negative");
+		exception.Message.ShouldContain("coordinate");
+	}
+}
